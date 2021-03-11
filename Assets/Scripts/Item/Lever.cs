d9276b770a47e763003b34479c6c0e8e2dfdd751
Flor Santos <florsantosbueno@gmail.com>
2021-03-11 11:18:11 +0100

    Add items to level 2

---
+++ Assets/Scripts/Item/Lever.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private Animator anim;
+    private bool hasPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            if (anim != null)
+            {
+                anim.Play("Lever");
+                anim.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hasPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hasPlayer = false;
+        }
+    }
+}
